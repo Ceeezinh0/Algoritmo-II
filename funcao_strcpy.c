9fc3f2dd43dc4c2e965d287094fd70a92b3fc5b2
Cristiano Marques Silva <118139423+Cristisiuuu@users.noreply.github.com>
2023-08-28 22:09:56 -0300

    Função string

---
+++ funcao_strcpy.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+int main() {
+ char str1[40], str2[40];
+ strcpy (str2,"ALGORITMOS I");
+ strcpy(str1,str2);
+ printf("%s",str1);
+ printf("\n%s",str2);
+ return 0;
+} 
