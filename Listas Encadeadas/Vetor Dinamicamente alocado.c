6cbb755537eedb9211cf8a898ec5d5379330ea8a
Cristiano Marques Silva <118139423+Cristisiuuu@users.noreply.github.com>
2023-11-06 15:37:51 -0300

    Update Vetor Dinamicamente alocado.c

--- Listas Encadeadas/Vetor Dinamicamente alocado.c
+++ Listas Encadeadas/Vetor Dinamicamente alocado.c
@@ -6,25 +6,26 @@ int main () {
   int *vet;
   
   printf ("\nQuantidade de valores do vetor: ");
-  scanf ("%d",&qte);
+  scanf ("%d", &qte);
   
-  if ((vet = (int *)malloc(qte*sizeof(int))) == NULL) {
-  	//malloc => aloca dinamicamente uma quantidade específica de memória, que nesse caso é qte*sizeof(int)
-	// qte*sizeof(int) => calcula o tamanho necessário em bytes para armazenar um vetor de qte elementos (inteiro)   
-    printf ("Memoria insuficiente");
-    exit (1);
+  if ((vet = (int *)malloc(qte * sizeof(int))) == NULL) {
+    // A função malloc aloca dinamicamente uma quantidade específica de memória,
+    // que nesse caso é qte * sizeof(int), calculando o tamanho necessário em bytes.
+    printf ("Memória insuficiente");
+    exit(1);
   }
   
   printf ("\nDigite os valores do vetor:");
   
-  for (i = 0; i<qte; i++) {
-    printf ("\nDigite o %d. elemento: ",i+1);
-    scanf ("%d",&vet[i]);
+  for (i = 0; i < qte; i++) {
+    printf ("\nDigite o %d. elemento: ", i + 1);
+    scanf ("%d", &vet[i]);
   }
   
   printf ("\nImprimindo os valores do vetor:");
-  for (i = 0; i<qte; i++)
-    printf ("\nvet[%d] = %d",i+1,vet[i]);
-  free (vet);
-  //libera a memória alocada dinamicamente
+  for (i = 0; i < qte; i++)
+    printf ("\nvet[%d] = %d", i + 1, vet[i]);
+  
+  free(vet);
+  // Libera a memória alocada dinamicamente.
 }
